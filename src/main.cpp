a2f49cfe1a7fd84f10ea87c91e4a1485733d2d93
Miquel Beltran <miquelbeltran@gmail.com>
2016-11-05 22:49:20 +0100

    :sparkles: Moving the ultrasonic sensor left and right

--- src/main.cpp
+++ src/main.cpp
@@ -1,52 +1,51 @@
 #include <Arduino.h>
 #include <Movement.h>
 #include <Led.h>
+#include <Servo.h>
+
+Servo servo;
 
 void setup() {
     Serial.begin(9600);
     setupMovement();
+    setupLed();
+    servo.attach(3);
+    servo.write(90);
 }
 
-int incomingByte = 0;
-
 void loop() {
-    if (Serial.available() > 0) {
-        incomingByte = Serial.read();
-        switch (incomingByte) {
-            case 'A':
-                light(true);
-                break;
-            case 'B':
-                light(false);
-                break;
-            case 'U':
-                forward(100);
-                break;
-            case 'D':
-                back(100);
-                break;
-            case 'L':
-                left(100);
-                break;
-            case 'R':
-                right(100);
-                break;
-            default:
-                delay(100);
-        }
-    } else {
-        delay(100);
-    }
-    // forward(1000);
-    // stop(1000);
-    // left(300);
-    // stop(1000);
-    // forward();
-    // delay(1000);
-    // back();
-    // delay(1000);
-    // left();
-    // delay(1000);
-    // right();
-    // delay(1000);
+    servo.write(90);
+    delay(1000);
+    servo.write(135);
+    delay(1000);
+    servo.write(45);
+    delay(1000);
+    
+    //if (Serial.available() > 0) {
+        //int incomingByte = Serial.read();
+        //switch (incomingByte) {
+            //case 'A':
+                //light(true);
+                //break;
+            //case 'B':
+                //light(false);
+                //break;
+            //case 'U':
+                //forward(100);
+                //break;
+            //case 'D':
+                //back(100);
+                //break;
+            //case 'L':
+                //left(100);
+                //break;
+            //case 'R':
+                //right(100);
+                //break;
+            //default:
+                //delay(100);
+        //}
+    //} else {
+        //delay(100);
+    //}
 }
